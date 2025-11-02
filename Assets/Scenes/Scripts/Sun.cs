0d00faeb64ee92caad6b5b1152317545889a7ad7
Lalo104lucky <20223tn021@utez.edu.mx>
2025-11-01 23:13:12 -0600

    [ADD] Sunflower funcionando casi al 100

--- Assets/Scenes/Scripts/Sun.cs
+++ Assets/Scenes/Scripts/Sun.cs
@@ -4,18 +4,17 @@ using UnityEngine.InputSystem;
 public class Sun : MonoBehaviour
 {
 
-    private float dropToYPos;
+    public float dropToYPos;
     private float speed = 2f;
     private void Start()
     {
-        transform.position = new Vector3(Random.Range(-4.8f, 6f), 6f, 0f);
-        dropToYPos = Random.Range(2f, -3f);
+
         Destroy(gameObject, Random.Range(6, 12));
     }
 
     private void Update()
     {
-        if (transform.position.y >= dropToYPos)
+        if (transform.position.y > dropToYPos)
             transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
     }
 }
--- Assets/Scenes/Scripts/SunSpawner.cs
+++ Assets/Scenes/Scripts/SunSpawner.cs
@@ -11,7 +11,8 @@ public class SunSpawner : MonoBehaviour
 
     void SpawnSun()
     {
-        Instantiate(sunObject);
+        GameObject mySun =  Instantiate(sunObject, new Vector3(Random.Range(-4.8f, 6f), 6f, 0f), Quaternion.identity);
+        mySun.GetComponent<Sun>().dropToYPos = Random.Range(2f, -3f);
         Invoke("SpawnSun", Random.Range(4, 10));
     }
 }
